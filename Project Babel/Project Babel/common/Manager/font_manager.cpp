e7c3e88a512fe4a71171ac23514dee158e0f2e4b
ZexonPrime <zexonprime@outlook.com>
2015-04-23 19:09:28 -0700

    text wip wip

--- Project Babel/Project Babel/common/Manager/font_manager.cpp
+++ Project Babel/Project Babel/common/Manager/font_manager.cpp
@@ -16,6 +16,21 @@ void FontManager::Init(GameObject * g_obj)
 
 
 
+
+	texture_atlas_t *atlas = texture_atlas_new(512, 512, 1);
+	const char * filename = "data/fonts/Vera.ttf";
+	wchar_t *text = L"IM GONNA FUCKING KILL MYSELF";
+	vertex_buffer_t *buffer = vertex_buffer_new("vertex:3f,tex_coord:2f,color:4f");
+	texture_font_t *font = texture_font_new_from_file(atlas, 128, filename);
+	vec2 pen = { { 200, 400 } };
+	vec4 black = { { 0.5, 0.5, 0.5, 1 } };
+
+	g_obj->GetFontList()->GetFont()->add_text(buffer, font, text, &black, &pen);
+	
+
+
+
+
 	this->UnbindCreate();
 
 
@@ -33,7 +48,7 @@ void FontManager::Clean()
 
 }
 
-vertex_buffer_t *buffer;
+
 
 void FontManager::Render(Controller * ctrl, GameObject * g_obj, Tooltip *t_tip)
 {
@@ -44,26 +59,15 @@ void FontManager::Render(Controller * ctrl, GameObject * g_obj, Tooltip *t_tip)
 
 	this->SetColor(1.f, 1.f, 1.f);
 	g_obj->GetFontList()->GetFont()->Print("Text + test II", 200, 400, 45);
-
-	size_t i;
-	texture_font_t *font = 0;
-	texture_atlas_t *atlas = texture_atlas_new(512, 512, 1);
-	const char * filename = "data/fonts/Vera.ttf";
-	wchar_t *text = L"A Quick Brown Fox Jumps Over The Lazy Dog 0123456789";
-	buffer = vertex_buffer_new("vertex:3f,tex_coord:2f,color:4f");
-	vec2 pen = { { 5, 400 } };
-	vec4 black = { { 0, 0, 0, 1 } };
-	for (i = 7; i < 27; ++i)
-	{
-		font = texture_font_new_from_file(atlas, i, filename);
-		pen.x = 5;
-		pen.y -= font->height;
-		texture_font_load_glyphs(font, text);
-		g_obj->GetFontList()->GetFont()->add_text(buffer, font, text, &black, &pen);
-		texture_font_delete(font);
-	}
-	glBindTexture(GL_TEXTURE_2D, atlas->id);
 	
+
+
+	
+	
+
+
+
+
 
 	this->fText->Render(g_obj, this, ctrl);
 
