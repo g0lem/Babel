623a3d90675e8dc0dc5f13d8fd6d8a01c99c62aa
ZexonPrime <zexonprime@outlook.com>
2015-04-25 21:06:51 -0700

    lel

--- Project Babel/Project Babel/common/AI/enemy.cpp
+++ Project Babel/Project Babel/common/AI/enemy.cpp
@@ -37,7 +37,7 @@ void Enemy::Render(Controller * ctrl, ScreenUniformData * u_data, GameObject * g
 	this->last_position = this->p_attributes->position;
 	u_data->ApplyMatrix(Translation(p_attributes->position * p_attributes->scale + g_obj->GetScroller()->GetOffset())*
 		Scale(p_attributes->scale));
-	u_data->SetAmbientLight(color * (glm::abs(1.5f*glm::sin(glfwGetTime()*5.0f)) + 0.6f));
+	//u_data->SetAmbientLight(color * (glm::abs(1.5f*glm::sin(glfwGetTime()*5.0f)) + 0.6f));
 
 
 
